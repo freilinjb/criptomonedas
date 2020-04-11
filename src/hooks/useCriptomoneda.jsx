28ae0fb0ab30e4cbad125a3422b11b2e86ec8aeb
freilinjb <freilinjb@gmail.com>
2020-04-11 19:04:07 -0400

    mostrando consulta al select y mostrando - Probado

--- src/hooks/useCriptomoneda.jsx
+++ src/hooks/useCriptomoneda.jsx
@@ -24,9 +24,6 @@ const Select = styled.select`
 
 const useCriptomoneda = (label,stateInicial,opciones) => {
 
-    console.log(opciones);
-     
-
     //State de nuestro custom hook
     const [state,setState] = useState(stateInicial);
 
@@ -41,9 +38,9 @@ const useCriptomoneda = (label,stateInicial,opciones) => {
                     value={state}>
 
                     <option value="">--Seleccione --</option>
-                    {/* {opciones.map(opcion => (
-                        <option key={opcion.codigo} value={opcion.codigo}>{opcion.nombre}</option>
-                    ))} */}
+                    {opciones.map(opcion => (
+                        <option key={opcion.CoinInfo.id} value={opcion.CoinInfo.Name}>{opcion.CoinInfo.FullName}</option>
+                    ))}
                 </Select>
             </div>
         </Fragment>
